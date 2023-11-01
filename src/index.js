bdc59dba037dd8dc90e7e0977635760521278494
markos <markos.paltser@voco.ee>
2023-11-01 13:22:29 +0200

    user can change bio and update/delete profile picture

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ const path = require('path');
 const hbs = require('hbs');
 const bcrypt = require('bcrypt');
 const mysql_connect = require('./mysql_connect'); // Require the database module
+const multer = require('multer');
+const storage = multer.memoryStorage(); // Use memory storage for file handling
+const sharp = require('sharp');
+
+
+const upload = multer({storage: storage});
+const methodOverride = require('method-override');
+
 
 const templatePath = path.join(__dirname, '../templates');
 app.use(express.static('public'));
@@ -13,7 +21,10 @@ app.use(express.static('templates'));
 app.use(express.json());
 app.set('view engine', 'hbs');
 app.set('views', templatePath);
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+
 
 app.use(
     session({
@@ -22,6 +33,16 @@ app.use(
         saveUninitialized: true,
     })
 );
+
+function isAuthenticated(req, res, next) {
+    if (req.session.isAuthenticated) {
+        // User is authenticated, proceed to the route handler
+        next();
+    } else {
+        // User is not authenticated, redirect to the login page or handle it accordingly
+        res.redirect('/login');
+    }
+}
 
 app.get('/', (req, res) => {
     // Check if the user is authenticated
@@ -75,7 +96,7 @@ app.post('/login', async (req, res) => {
     mysql_connect.query(selectUserQuery, [name], async (err, results) => {
         if (err) {
             console.error('Error retrieving user:', err);
-            res.render('login', { errorMessage: 'An error occurred' });
+            res.render('login', {errorMessage: 'An error occurred'});
         } else if (results.length > 0) {
             const user = results[0];
 
@@ -84,15 +105,17 @@ app.post('/login', async (req, res) => {
 
             if (passwordMatch) {
                 req.session.isAuthenticated = true;
+                req.session.userId = user.id;
+
                 // Passwords match, allow login
                 res.render('home');
             } else {
                 // Passwords do not match
-                res.render('login', { errorMessage: 'Wrong Password' });
+                res.render('login', {errorMessage: 'Wrong Password'});
             }
         } else {
             // User not found
-            res.render('login', { errorMessage: 'User not found' });
+            res.render('login', {errorMessage: 'User not found'});
         }
     });
 });
@@ -104,9 +127,160 @@ app.get('/logout', (req, res) => {
 
 // home pages stuff
 
-app.get('/profile', (req, res) => {
-    res.render('profile');
 
+app.get('/profile', (req, res) => {
+    if (req.session.isAuthenticated) {
+        // Assuming you have the user's ID stored in the session
+        const userId = req.session.userId;
+
+        console.log('User ID from session:', userId);
+
+        // Fetch the user's profile data from the database
+        const selectUserProfileQuery = 'SELECT * FROM users WHERE id = ?';
+
+        mysql_connect.query(selectUserProfileQuery, [userId], (err, results) => {
+            if (err) {
+                console.error('Error retrieving user profile:', err);
+                res.status(500).send('Error retrieving user profile.');
+            } else if (results.length > 0) {
+                const userProfile = results[0];
+
+                if (userProfile.picture) {
+                    const pictureBase64 = userProfile.picture.toString('base64');
+                    res.render('profile', { userProfile: { ...userProfile, picture: pictureBase64 } });
+                } else {
+                    // Handle the case when there's no picture available
+                    res.render('profile', { userProfile });
+                }
+            } else {
+                res.status(404).send('User not found');
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+
+// bio stuff
+
+app.get('/profile/bio', isAuthenticated, (req, res) => {
+    const userId = req.session.userId;
+
+    // Retrieve the user's bio from the database based on their ID
+    const selectUserBioQuery = 'SELECT bio FROM users WHERE id = ?';
+
+    mysql_connect.query(selectUserBioQuery, [userId], (err, results) => {
+        if (err) {
+            console.error('Error retrieving user bio:', err);
+            res.status(500).send('Error retrieving user bio.');
+        } else if (results.length > 0) {
+            const userBio = results[0].bio;
+            res.json({bio: userBio});
+        } else {
+            res.status(404).send('User not found');
+        }
+    });
+});
+
+app.post('/profile/bio', isAuthenticated, (req, res) => {
+    const userId = req.session.userId;
+    const newBio = req.body.bio; // Assuming the bio is sent in the request body
+
+    // Update the user's bio in the database
+    const updateBioQuery = 'UPDATE users SET bio = ? WHERE id = ?';
+
+    mysql_connect.query(updateBioQuery, [newBio, userId], (err) => {
+        if (err) {
+            console.error('Error updating user bio:', err);
+            res.status(500).send('Error updating user bio.');
+        } else {
+            // Redirect the user back to their profile page
+            res.redirect('/profile');
+        }
+    });
+});
+
+app.put('/profile/bio', isAuthenticated, (req, res) => {
+    const userId = req.session.userId;
+    const newBio = req.body.bio; // Assuming you send the new bio in the request body
+
+    // Update the user's bio in the database
+    const updateBioQuery = 'UPDATE users SET bio = ? WHERE id = ?';
+
+    mysql_connect.query(updateBioQuery, [newBio, userId], (err) => {
+        if (err) {
+            console.error('Error updating user bio:', err);
+            res.status(500).send('Error updating user bio.');
+        } else {
+            res.status(200).send('Bio updated successfully');
+        }
+    });
+});
+
+app.delete('/profile/bio', isAuthenticated, (req, res) => {
+    const userId = req.session.userId;
+
+    // Remove the user's bio in the database
+    const deleteBioQuery = 'UPDATE users SET bio = NULL WHERE id = ?';
+
+    mysql_connect.query(deleteBioQuery, [userId], (err) => {
+        if (err) {
+            console.error('Error deleting user bio:', err);
+            res.status(500).send('Error deleting user bio.');
+        } else {
+            res.status(204).send(); // A status of 204 indicates success with no content
+        }
+    });
+});
+
+// picture stuff
+
+app.post('/profile/picture', isAuthenticated, upload.single('profilePicture'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('Please select a valid image file.');
+    }
+
+    const userId = req.session.userId;
+    const profilePictureData = req.file.buffer;
+
+    // Resize the image to 128x128 pixels
+    sharp(profilePictureData)
+        .resize(128, 128)
+        .toBuffer()
+        .then((resizedData) => {
+            // Update the user's profile picture in the database
+            const updatePictureQuery = 'UPDATE users SET picture = ? WHERE id = ?';
+
+            mysql_connect.query(updatePictureQuery, [resizedData, userId], (err) => {
+                if (err) {
+                    console.error('Error updating user picture:', err);
+                    return res.status(500).send('Failed to update profile picture.');
+                }
+
+                res.status(200).send('Profile picture updated successfully.');
+            });
+        })
+        .catch((err) => {
+            console.error('Error resizing image:', err);
+            return res.status(500).send('Failed to resize profile picture.');
+        });
+});
+app.post('/profile/picture/delete', isAuthenticated, (req, res) => {
+    const userId = req.session.userId;
+    console.log('kasutaja ' + userId)
+    // Remove the user's profile picture in the database
+    const deletePictureQuery = 'UPDATE users SET picture = NULL WHERE id = ?';
+
+    mysql_connect.query(deletePictureQuery, [userId], (err) => {
+        if (err) {
+            console.error('Error deleting user picture:', err);
+            return res.status(500).send('Failed to remove profile picture.');
+        }
+
+        res.status(200).send('Profile picture removed successfully.');
+    });
 });
 
 app.listen(3000, () => {
